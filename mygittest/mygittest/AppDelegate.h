252fef4e8d11c1115861d6c2e4a8baf42e910421
shixuanlei <stoneobs@icloud.com>
2016-03-14 15:04:37 +0800

    新建了xcode

---
+++ mygittest/mygittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mygittest
+//
+//  Created by stoneobs on 16/3/14.
+//  Copyright © 2016年 stoneobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
