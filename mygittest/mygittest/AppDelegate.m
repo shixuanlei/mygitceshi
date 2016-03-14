974f84720055c814ae251149408dd56ae12b508a
shixuanlei <stoneobs@icloud.com>
2016-03-14 00:21:50 -0700

    zzy tibia de

--- mygittest/mygittest/AppDelegate.m
+++ mygittest/mygittest/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     //我要提交一个新版本
+    //zzy
     return YES;
 }
 
